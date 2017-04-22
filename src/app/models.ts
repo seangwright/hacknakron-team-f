4b5865f7a36894fc057266881240933b42fc85bb
Seany Computer <sean@wiredviews.com>
2017-04-22 13:55:03 -0400

    refactor(Models): add LucCategory, fields to ParcelResponse, rename LucOption

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,8 +1,21 @@
-export interface LUCOption {
+export interface LucOption {
     id: number;
     description: string;
 }
 
 export interface ParcelResponse {
+    number: string;
+    address: string;
+    postal: string;
+    areaAcres: number;
+    areaSqft: number;
+    lucId: number;
+    appraisal: number;
+    lastSaleDate: Date;
+    lastSaleAmount: number;
+}
 
+export interface LucCategory {
+    code: string;
+    name: string;
 }
