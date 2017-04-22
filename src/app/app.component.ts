0d80b3f212d365611db81367b63f405bf7970137
Seany Computer <sean@wiredviews.com>
2017-04-22 13:27:13 -0400

    feat(AppComponent): add skeleton for multi-select functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 
 import { ApiService } from './api.service';
 
@@ -7,10 +8,23 @@ import { ApiService } from './api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  optionsModel: number[];
 
+  myOptions: IMultiSelectOption[] = [];
   constructor(
     private apiService: ApiService
   ) { }
+
+  ngOnInit() {
+    this.myOptions = [
+      { id: 1, name: 'Option 1' },
+      { id: 2, name: 'Option 2' },
+    ];
+  }
+  onChange() {
+    console.log(this.optionsModel);
+  }
+
 }
