a1c36080a392e6fab10210f438a43b3a3a1f342a
Seany Computer <sean@wiredviews.com>
2017-04-22 13:26:54 -0400

    feat(AppModule): add multi select module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
+import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
+
 import { ApiService } from './api.service';
 import { AppComponent } from './app.component';
 
@@ -16,6 +18,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule,
 
+    MultiselectDropdownModule,
     BsDropdownModule.forRoot()
   ],
   providers: [
