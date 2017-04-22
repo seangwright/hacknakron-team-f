1fb1bfef22bf8ca5f333af0d7060ee2873ec88ec
Seany Computer <sean@wiredviews.com>
2017-04-22 11:31:51 -0400

    feat(AppComponent): add ApiService dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ApiService } from './api.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 }
